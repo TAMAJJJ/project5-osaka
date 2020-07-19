c8519ce7597014ad48b9bbd3f5d0ea21363597ca
efreling <67718969+efreling@users.noreply.github.com>
2020-07-19 05:20:27 -0400

    Write more index html, add a little bit of CSS (really rough rn)

--- index.php
+++ index.php
@@ -31,12 +31,17 @@ include "inc/html-top.php";
 
 				<div class="column_2">
 					<h3>What is Digital Media Studies?</h3>
-					<p>Lorem Ipsum</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					
 				</div>
 
 				<div class="column_3">
-					
+					<a href="studentlist.php" class="meet">Meet the Students!</a>					
 				</div>
 			</article>
 
