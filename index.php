68e9613edd3da2596d6f1079bcf81041caaa725d
Bonnie Yang <bonniehyang@gmail.com>
2020-07-30 16:52:27 -0400

    changes to css on main page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<?php 
+<?php
 $customCSS = "<link rel='stylesheet' href='css/main.css'> ";
 include "inc/html-top.php";
 ?>
@@ -7,9 +7,8 @@ include "inc/html-top.php";
 
 	<header>
 
-				
-				<h1>CSC 174 Students</h1>
-				<h1 class="h1_index">In Digital Media</h1>
+
+				<h1>CSC 174 Students<br>In Digital Media</h1>
 
 
 	</header>
@@ -25,11 +24,11 @@ include "inc/html-top.php";
                 <div class="column_2">
                     <h2>What is Digital Media Studies?</h2>
                     <p>Digital technology is changing how we do everything. More than ever, it is important to be a critical consumer and educated producer of digital media.Sometimes known as new media, digital media consists of computer-based, networked technologies that represent and imagine the world. It is the new standard for literacy in the 21st century.</p>
-                    
+
                 </div>
 
                 <div class="column_3">
-                    <a href="studentlist.php" class="button">Meet the Students!</a>                   
+                    <a href="studentlist.php" class="button">Meet the Students!</a>
                 </div>
 		<footer>
 			<p>CSC 174: Advanced Front-end Web - Project 3</p>
