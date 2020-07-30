c7de1a140370a43573e61866c2adad21cc449996
Michael B <mbashner@u.rochester.edu>
2020-07-30 18:12:56 -0400

    Clean up formatting on index.php

--- index.php
+++ index.php
@@ -1,40 +1,26 @@
-<?php
-$customCSS = "<link rel='stylesheet' href='css/main.css'> ";
-include "inc/html-top.php";
-?>
-
-
-	<header>
-
-
-				<h1>CSC 174 Students<br>In Digital Media</h1>
-
-
-	</header>
-
-	<div class="background">
-		<div class="container">
-
-				<div class="column_1">
-					<h2>An Introduction to Rettner Hall</h2>
-                    <iframe width="280" height="157" src="https://www.youtube.com/embed/CY7zJbmlI0I" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="youtube"></iframe>
-                </div>
-
-                <div class="column_2">
-                    <h2>What is Digital Media Studies?</h2>
-                    <p>Digital technology is changing how we do everything. More than ever, it is important to be a critical consumer and educated producer of digital media.Sometimes known as new media, digital media consists of computer-based, networked technologies that represent and imagine the world. It is the new standard for literacy in the 21st century.</p>
-
-                </div>
-
-                <div class="column_3">
-                    <a href="studentlist.php" class="button">Meet the Students!</a>
-                </div>
-		<footer>
-			<p>CSC 174: Advanced Front-end Web - Project 3</p>
-		</footer>
-
-		</div> <!-- container -->
-	</div> <!-- background -->
-
-<?php include "inc/scripts.php" ?>
-</body>
+<?php $customCSS = "<link rel='stylesheet' href='css/main.css'> ";
+include "inc/html-top.php"; ?>
+		<header>
+			<h1>CSC 174 Students<br>In Digital Media</h1>
+		</header>
+		<div class="background">
+			<div class="container">
+					<div class="column_1">
+						<h2>An Introduction to Rettner Hall</h2>
+						<iframe width="280" height="157" src="https://www.youtube.com/embed/CY7zJbmlI0I" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="youtube"></iframe>
+					</div>
+					<div class="column_2">
+						<h2>What is Digital Media Studies?</h2>
+						<p>Digital technology is changing how we do everything. More than ever, it is important to be a critical consumer and educated producer of digital media.Sometimes known as new media, digital media consists of computer-based, networked technologies that represent and imagine the world. It is the new standard for literacy in the 21st century.</p>
+					</div>
+					<div class="column_3">
+						<a href="studentlist.php" class="button">Meet the Students!</a>
+					</div>
+			<footer>
+				<p>CSC 174: Advanced Front-end Web - Project 3</p>
+			</footer>
+			</div> <!-- container -->
+		</div> <!-- background -->
+	<?php include "inc/scripts.php" ?>
+	</body>
+</html>
