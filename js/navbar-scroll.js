782cffd01fca8410898b31cb2d2ff4d498faf614
Bonnie Yang <bonniehyang@gmail.com>
2020-07-30 17:07:12 -0400

    changed js to fit new index height

--- js/navbar-scroll.js
+++ js/navbar-scroll.js
@@ -4,7 +4,7 @@ window.onscroll = function() {scrollFunction()};
 //code adapted from https://www.w3schools.com/howto/howto_js_navbar_shrink_scroll.asp
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
     document.getElementById("navbar").style.height = "40px";
     document.getElementById("navbar").style.backgroundColor = "rgba(0, 181, 229, 0.5)";
     document.getElementById("logo").style.fontSize = "20px";
