1b5fb9bfafad14003a5db4a9a7d5927749bec706
Michael B <mbashner@u.rochester.edu>
2020-08-03 17:33:20 -0400

    Added JavaScript to html-top.php

--- inc/html-top.php
+++ inc/html-top.php
@@ -8,6 +8,28 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Playfair+Display&family=Zilla+Slab:wght@500&display=swap">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap">
 		<?php echo $customCSS; ?>
+		<?php if(isset($formTitle) && $formTitle == "Update") { ?>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script>
+			$(document).ready(function(){
+				$("label input[type='file']").prop("type", "hidden");
+				document.getElementById("file").hidden = true;
+				document.getElementById("file").style = "display:none";
+				$(".photo-change").click(function(){
+					if($(this).prop("checked") == true){
+						$("label input[type='hidden']").prop("type", "file");
+						document.getElementById("file").hidden = false;
+						document.getElementById("file").style = "display:block";
+					}
+					else if($(this).prop("checked") == false){
+						$("label input[type='file']").prop("type", "hidden");
+						 document.getElementById("file").hidden = true;
+						 document.getElementById("file").style = "display:none";
+					}
+				});
+			});
+		</script>
+	<?php } ?>
 	</head>
 	<body>
 		<nav class="banner" id="navbar">
