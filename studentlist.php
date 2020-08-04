dd87e359440a3dbd7a6ee5dbca9079a6793600f4
Emely Rosa <erosa4@u.rochester.edu>
2020-08-03 21:14:25 -0400

    Delete button and file
    Added delete button and created the file

--- studentlist.php
+++ studentlist.php
@@ -29,7 +29,9 @@ include "inc/html-top.php"; ?>
 			</div>
 			<div class="grid_item_link">
 				<a class="button" href="<?php echo $row['website']; ?>"><?php echo $row['firstname']; ?>'s Website</a>
-				<a class="button edit" href="edit.php?id=<?php echo htmlspecialchars($row['id']); ?>">Edit/Delete</a></td>
+				<a class="button edit" href="edit.php?id=<?php echo htmlspecialchars($row['id']); ?>">Edit</a></td>
+				<a class="button delete" href="delete.php?id=<?php echo htmlspecialchars($row['id']); ?>">Delete</a></td>
+
 			</div>
 			<?php	} // close the loop ?>
 			<footer class="list-footer">
