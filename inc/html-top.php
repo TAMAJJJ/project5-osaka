8ec02faaf38e7b3e7f4f1e9d4b67b07e2f458459
Dan <tamajessica18@gmail.com>
2020-08-06 09:05:17 -0400

    change title

--- inc/html-top.php
+++ inc/html-top.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Osaka | Project 4</title>
+		<title>Osaka | Project 5</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/styles.css">
 		<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Playfair+Display&family=Zilla+Slab:wght@500&display=swap">
