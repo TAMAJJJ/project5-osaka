48a035b41e8fd6089d8e86a742f92a03f0113952
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-08-07 21:52:12 -0400

    font update

--- register.php
+++ register.php
@@ -143,7 +143,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                                     <!-- <input type="reset" class="btn btn-danger" value="Reset"> -->
                                 </div>
 
-                                <p>Already have an account? <a href="login.php">Login here</a></p>
+                                <p>Already have an account? <a class = "already-account" href="login.php">Login here</a></p>
                             </form>
                     </div>
                 </div>         
